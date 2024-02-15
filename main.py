89b08dd5e237e7a56d7254424d4ebda2b5524559
saschockk0 <saschockk@gmail.com>
2024-02-15 19:14:29 +0300

    swapping file added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 ##
 import shutil
 import subprocess
+import time
+
 import ffmpeg
 import os
 import typer
@@ -46,8 +48,8 @@ def diff_files(source_folder: Path, dest_dir: Path, source_pattern: str = "*", d
 # Сравнение и удаление файлов в папках
 def diff_and_remove_files(source_folder: Path, dest_dir: Path, source_pattern: str = "*", dest_pattern: str = "*"):
     to_remove = diff_files(source_folder, dest_dir, source_pattern=source_pattern, dest_pattern=dest_pattern)
-    if len(to_remove) > 0:
-        kill_vlc()
+    # if len(to_remove) > 0:
+        # kill_vlc()
 
     for dest_dir_file in to_remove:
         print(f"Removing file {dest_dir_file}")
@@ -102,7 +104,10 @@ def vlc_alive() -> bool:
     return False
 
 
-
+def test_vlc(video: Path):
+    print("Starting VLC for test")
+    subprocess.Popen(["C:/Program Files/VideoLAN/VLC/vlc.exe", video, "-R"])
+    return print("Successfully started VLC test")
 def start_vlc(video: Path):
     print("Starting VLC")
     subprocess.Popen(
@@ -124,7 +129,14 @@ def kill_vlc():
     except: #Код ошибки?
         return print("Error occurred during killing VLC process")
     else:
-        return  print("Successfully killed VLC process")
+        return print("Successfully killed VLC process")
+
+
+# def switch_video(temp_final_video: Path, final_video: Path, vlc_alive: bool):
+#     if vlc_alive:
+#         kill_vlc()
+#     final_video
+#     start_vlc(final_video)
 
 
 @app.command()
@@ -135,6 +147,7 @@ def main(source_dir: Path, dest_dir:  Path):
     converted_videos_folder = dest_dir / "Converted"
     final_folder = converted_videos_folder / "Final"
     final_video = final_folder / "Final.mp4"
+    temp_final_video = final_folder / "TempFinal.mp4"
 
     if not final_folder.exists():
         final_folder.mkdir(exist_ok=True, mode=777, parents=True)
@@ -146,12 +159,18 @@ def main(source_dir: Path, dest_dir:  Path):
     convert_rotate(copied_files, converted_videos_folder, suffix="mp4")
 
     if len(copied_files) > 0 or len(deleted_files) > 0 or not final_video.exists():
+        concatenate(get_files(converted_videos_folder, pattern="*.mp4"), temp_final_video)
         kill_vlc()
-        concatenate(get_files(converted_videos_folder, pattern="*.mp4"), final_video)
-        start_vlc(final_video)
+        if final_video.exists():
+            time.sleep(5)
+            os.remove(final_video)
+        os.rename(temp_final_video, final_video)
+        # start_vlc(final_video)
+        test_vlc(final_video)
     else:
         if vlc_alive() is False:
-            start_vlc(final_video)
+            # start_vlc(final_video)
+            test_vlc(final_video)
 
 
 if __name__ == "__main__":
